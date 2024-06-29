f0fbe0771c23a57a3a81b93c87360393b24c7cd9
bruce <euryanspike.pro@gmaiul.com>
2024-06-28 21:07:13 -0300

    integração do banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 
+//banco de dados
+import db from './config/database';
+
 const app = express();
 const PORT = 8080;
 
@@ -12,3 +15,7 @@ app.get('/home', (req, res) => {
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 })
+
+//status banco de dados
+db.on('open', () => console.log('bank status: on-line'))
+db.once('close', () => console.log('bank status: off-line'))
